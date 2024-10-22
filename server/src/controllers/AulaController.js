7887e20cf9f61c54c71bc9d274eab1ddfd998533
Vitor Ribeiro <vitor.r.fonseca4@gmail.com>
2024-10-21 21:42:21 -0300

    Finalizando Crud read, update e delete

--- server/src/controllers/AulaController.js
+++ server/src/controllers/AulaController.js
@@ -1,4 +1,4 @@
-import { createAula } from "../models/AulaModel.js";
+import { createAula,readAulas,updateAula,deleteAula } from "../models/AulaModel.js";
 
 export async function cadastroAula(req,res){
     //Criando constante aula
@@ -6,6 +6,44 @@ export async function cadastroAula(req,res){
     try {
         //Declarando status com o codigo da resposta e resposta como JSON
         const [status,resposta] = await createAula(aula);
+        res.status(status).json(resposta);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+export async function mostrandoAulas(req,res) {
+    console.log('AulaController mostrandoAulas');
+    try {
+        const[status,resposta] = await readAulas();
+        res.status(status).json(resposta);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+export async function atualizandoAula(req,res) {
+    console.log('AulaController atualizandoAula');
+    const {id} = req.params;
+    const aula = req.body;
+
+    try {
+        const [status,resposta] = await updateAula(aula,id);
+        res.status(status).json(resposta);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+export async function excluindoAula(req,res) {
+    console.log('AulaController atualizandoAula');
+    const {id} = req.params;
+
+    try {
+        const [status,resposta] = await deleteAula(id);
         res.status(status).json(resposta);
     } catch (error) {
         console.log(error);
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
 //Importando pacote express
 import express from 'express';
 
-import { cadastroAula } from './controllers/AulaController.js';
+import { cadastroAula, mostrandoAulas,atualizandoAula, excluindoAula } from './controllers/AulaController.js';
 
 //Intanciando objeto express
 const app = express();
 const porta = 5000;
 
+//Configurando comunicação em JSON
 app.use(express.json());
 
 //Rota de teste da API
@@ -16,25 +17,10 @@ app.get('/',(req,res)=>{
 
 //Rotas de CRUD de aulas
 app.post('/aulas',cadastroAula);
-
-app.get('/aulas',(req,res)=>{
-    res.status(200).json(
-        [
-            {
-              "id": "1300",
-              "data": "2024-08-29T03:00:00.000Z",
-              "data_hora_inicio": "2024-08-29T21:00:00.000Z",
-              "data_hora_fim": "2024-08-30T01:00:00.000Z",
-              "turma": "EMP-NBM-03",
-              "instrutor": "JOEL HERBERT BARBOSA SILVA",
-              "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
-              "ambiente": "VTRIA-3-SALA-3004",
-              "chave": null
-            }
-        ]
-    );
-})
+app.get('/aulas',mostrandoAulas);
+app.put('/aulas/:id',atualizandoAula);
+app.delete('/aulas/:id',excluindoAula);
 
 app.listen(porta, ()=>{
     console.log(`API funcionando na porta ${porta}`)
-})
+});
